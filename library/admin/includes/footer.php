b1f854d40a7c47f0baa75dd72a8023234687b14f
vaibhavig <vaibhavigovankop@gmail.com>
2023-07-15 15:56:09 +0530

    first commit

---
+++ library/admin/includes/footer.php
@@ -0,0 +1,11 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; <?php echo date('Y');?> Online Library Management System |<a href="https://github.com/vaibhavigvp" target="_blank"  > Designed by : Vaibhavi Govankop</a> 
+                </div>
+                
+
+            </div>
+        </div>
+    </section>
